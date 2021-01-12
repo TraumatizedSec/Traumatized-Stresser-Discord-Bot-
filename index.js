db2ccd154832e54e63087a6b8c945ac5d89e71b7
Devious <74839733+DeviousTheDev@users.noreply.github.com>
2021-01-12 10:22:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-const fs = require("fs");  client.user.setPresence({ game: { name: `the bot is working`, type: 'WATCHING' }, status: 'online' });
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+//Bot on ready
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!\nServer Count: ${client.guilds.size}`);
+  client.user.setPresence({ game: { name: `the bot is working`, type: 'WATCHING' }, status: 'online' });
+});
+
+//Command handler
+
+client.on('message', async (message) => {
+    if (message.author.bot) return;
+    if (message.content.indexOf(config.prefix) !== 0) return;
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+  });
+
+  
+client.login('VERY-SECRET-TOKEN');
