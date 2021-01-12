1984b680077b49a226e28f85fea5bee0690413eb
Devious <74839733+DeviousTheDev@users.noreply.github.com>
2021-01-12 14:20:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ client.on('message', async (message) => {
     if (message.content.indexOf(config.prefix) !== 0) return;
     const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
-  });
+});
 
-var DORKAPI = "";
+
   
 client.login('VERY-SECRET-TOKEN');
