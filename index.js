dd623831d13d812934f717571fa6f5b9f3681cac
Devious <74839733+DeviousTheDev@users.noreply.github.com>
2021-01-12 15:06:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require("./src/config.js");
+const crud = require("./src/crud.js");
 
 //Bot on ready
 
@@ -12,18 +13,24 @@ client.on('ready', () => {
 //Command handler
 
 client.on('message', async (message) => {
-    if (message.author.bot) return;
-    if (message.content.startsWith(config.prefix)) {
-      let split = message.content.split(" ");
-      let i = 0;
-      split.forEach(e => {
-        config.CurrentMSG.arg[i] = e;
-        i++;
-      })
-      config.CurrentMSG.cmd = config.CurrentMSG.arg[0];
-    } else {
-      
-    }
+  if (message.author.bot) return;
+  if (message.content.startsWith(config.prefix)) {
+    let split = message.content.split(" ");
+    let i = 0;
+    split.forEach(e => {
+      config.CurrentMSG.arg[i] = e;
+      i++;
+    })
+    config.CurrentMSG.cmd = config.CurrentMSG.arg[0];
+    //LOG COMMAND
+  } else {
+    //LOG MESSAGE
+  }
+
+  if(crud.isRegistered(config.CurrentUser.Discord_id)) {
+
+  }
+
 });
 
 
