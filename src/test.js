be991c205b17f53b763b624066dff09d765ff881
Devious <74839733+DeviousTheDev@users.noreply.github.com>
2021-01-12 18:48:19 -0500

    Update test.js

--- src/test.js
+++ src/test.js
@@ -1,4 +1,5 @@
 var crud = require("./crud.js");
+const boot_management = require("./boot_management.js");
 
 /*
 THIS SCRIPT IS TO TEST FUNCTION IN THIS PROJECT!
@@ -6,4 +7,6 @@ THIS SCRIPT IS TO TEST FUNCTION IN THIS PROJECT!
 
 // console.log(crud.update(process.argv[2], process.argv[3], process.argv[4])) //UPDATE USER (SCRIPT USAGE SCRIPT.JS <DISCORD_ID> <STAT> <NEW_STAT>)
 // console.log(crud.user(process.argv[2], process.argv[3])) //GET USER STATS (SCRIPT USAGE SCRIPT.JS <DISCORD_ID> <STAT_TYPE>)
-console.log(crud.remove(process.argv[2])) //REMOVES USER (SCRIPT USAGE SCRIPT.JS <DISCORD_ID>)
+// console.log(crud.remove(process.argv[2])) //REMOVES USER (SCRIPT USAGE SCRIPT.JS <DISCORD_ID>)
+
+console.log()
