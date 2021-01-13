43a23580a484f81111563390686b965926e70a91
Devious <74839733+DeviousTheDev@users.noreply.github.com>
2021-01-13 15:40:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -104,6 +104,59 @@ client.on('message', async (message) => {
         footer: { text: `Traumatized | Created & Developed By: Traumatized Security | Main Server: ttps://discord.gg/9CAqV29Mjd`}
     }});
   }
+
+  function test() {
+    const exampleEmbed = {
+      color: 0x0099ff,
+      title: 'Some title',
+      url: 'https://discord.js.org',
+      author: {
+        name: 'Some name',
+        icon_url: 'https://i.imgur.com/wSTFkRM.png',
+        url: 'https://discord.js.org',
+      },
+      description: 'Some description here',
+      thumbnail: {
+        url: 'https://i.imgur.com/wSTFkRM.png',
+      },
+      fields: [
+        {
+          name: 'Regular field title',
+          value: 'Some value here',
+        },
+        {
+          name: '\u200b',
+          value: '\u200b',
+          inline: false,
+        },
+        {
+          name: 'Inline field title',
+          value: 'Some value here',
+          inline: true,
+        },
+        {
+          name: 'Inline field title',
+          value: 'Some value here',
+          inline: true,
+        },
+        {
+          name: 'Inline field title',
+          value: 'Some value here',
+          inline: true,
+        },
+      ],
+      image: {
+        url: 'https://i.imgur.com/wSTFkRM.png',
+      },
+      timestamp: new Date(),
+      footer: {
+        text: 'Some footer text here',
+        icon_url: 'https://i.imgur.com/wSTFkRM.png',
+      },
+    };
+    
+    channel.send({ embed: exampleEmbed });
+  }
 });
 
 client.login(config.BotInfo.Token);
