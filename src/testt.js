09d1c67adeeee909538940945ce83e3c2da395e5
Devious <74839733+DeviousTheDev@users.noreply.github.com>
2021-01-14 16:02:48 -0500

    Delete testt.js

--- src/testt.js
+++
@@ -1,6 +0,0 @@
-const extra = require("./extra.js");
-const current = new Date();
-
-// console.log(extra.test(process.argv[2]))
-
-console.log(current.getMonth()+1 + "/" + current.getDate() + "/" + current.getFullYear())
